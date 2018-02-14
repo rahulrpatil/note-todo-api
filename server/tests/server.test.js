d452fe65e4ebd5e426b1ac64fabdc7dad604a348
rahul patil <rahulpatil@rahuls-MacBook-Pro-2.local>
2018-02-14 19:53:10 +0530

    Test for DELETE /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -95,3 +95,34 @@ describe('GET /todos/:id', () => {
             .end(done);
     });
 });
+
+describe('DELETE /todos/:id', () => {
+    it('should return todo doc', (done) => {
+        request(app).delete(`/todos/${todos[0]._id.toHexString()}`)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.text).to.equal(todos[0].text);
+            })
+            .end((err, res) => {
+                if(err){
+                    return done(err);
+                }
+                Todo.find().then((todos) => {
+                    expect(todos.length).to.equal(1);
+                    done()
+                }).catch((e) => done(e));
+            })
+    });
+
+    it('should return 404 if todo not found', (done) => {
+        request(app).delete(`/todos/${(new ObjectID).toHexString()}`)
+            .expect(404)
+            .end(done);
+    });
+
+    it('should return 404 for non object ids', (done) => {
+        request(app).delete('/todos/12345')
+            .expect(404)
+            .end(done);
+    });
+});
