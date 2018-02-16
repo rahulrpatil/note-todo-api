b9fe32e31b6d94405888acf4e3dadafaec7c4f9a
rahul patil <rahulpatil@rahuls-MacBook-Pro-2.local>
2018-02-16 13:10:00 +0530

    Add GET /users/me and authenticate middleware

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ let {mongoose} = require('./db/mongoose');
 let {ObjectID} = require('mongodb');
 let {Todo} = require('./models/todo');
 let {User} = require('./models/user');
+let {authenticate} = require('./middleware/authenticate');
 
 let app = express();
 
@@ -111,6 +112,10 @@ app.post('/users', (req, res) => {
     }).catch((e) => {
         res.status(400).send(e);
     })
+});
+
+app.get('/users/me', authenticate, (req, res) => {
+   res.send(req.user);
 });
 
 app.listen(3000, () => {
